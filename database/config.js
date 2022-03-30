ce3fa4e7bb09f82891d8dc39eca669e916a87320
Chsebas98 <chsebas98@gmail.com>
2022-03-30 13:28:29 -0500

    maybe 7

--- database/config.js
+++ database/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const dbConnection = async () => {
 	try {
-		mongoose.connect(process.env.MONGODB_URI);
+		await mongoose.connect(process.env.MONGODB_URI);
 		console.log("BD Conectada");
 	} catch (error) {
 		console.log(erorr);
